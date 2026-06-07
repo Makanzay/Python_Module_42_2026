335637ef91fc3673e66e5ca538aed6f191c09bd4
Andrew <andrew.ludo@yahoo.com>
2026-06-07 23:01:02 +0200

    Base Module 5

--- module05/ex0/data_processor.py
+++ module05/ex0/data_processor.py
@@ -6,37 +6,69 @@ import abc
 
 
 class DataProcessor(abc.ABC):
-    @abc.abstractmethod
-    def process(self, data: typing.Any) -> typing.Any:
+    def __init__(self) -> None:
+        self.data = []
+
+    def output(self, data: typing.Any) -> typing.Any:
         pass
+
     @abc.abstractmethod
     def validate(self, data: typing.Any) -> bool:
         pass
+
     @abc.abstractmethod
     def ingest(self, data: typing.Any) -> None:
         pass
 
-class  NumericProcessor(DataProcessor):
-    def process(self, data: typing.Any) -> typing.Any
+
+class NumericProcessor(DataProcessor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def output(self, data: typing.Any) -> typing.Any:
         pass
+
     def validate(self, data: typing.Any) -> bool:
+        if isinstance(data, (int, float)):
+            return True
+        elif isinstance(data, list):
+            if all(isinstance(x, (int, float)) for x in data):
+                return True
+            else:
+                raise ValueError("Invalid data type for NumericProcessor:"
+                                 " list contains non-numeric values")
+        else:
+            raise ValueError("Invalid data type for NumericProcessor")
+        return False
+
+    def ingest(self, data: typing.Any) -> None:
         pass
+
 
 class TextProcessor(DataProcessor):
-    def process(self, data: typing.Any) -> typing.Any:
-        pass
-    def validate(self, data: typing.Any) -> bool:
+    def __init__(self) -> None:
+        super().__init__()
+
+    def output(self, data: typing.Any) -> typing.Any:
         pass
 
-class TextProcessor(DataProcessor):
-    def process(self, data: typing.Any) -> typing.Any:
-        pass
     def validate(self, data: typing.Any) -> bool:
+        return isinstance(data, str)
+
+    def ingest(self, data: typing.Any) -> None:
         pass
 
-    
+
 class LogProcessor(DataProcessor):
-    def process(self, data: typing.Any) -> typing.Any:
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = []
+
+    def output(self, data: typing.Any) -> typing.Any:
         pass
+
     def validate(self, data: typing.Any) -> bool:
-        pass
+        return isinstance(data, (int, float, str))
+
+    def ingest(self, data: typing.Any) -> None:
+        self.data.append(data)
